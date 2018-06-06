3be116ce559283d1998d6990cbba971cfa024df0
hadrianl <137150224@qq.com>
2018-06-06 15:00:06 +0800

    0.1.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = ['websocket-client',
             'pymongo']
 
 setup(name='huobitrade',
-      version='0.1.6',
+      version='0.1.7',
       description='huobi_api for python',
       long_description=long_description,
       long_description_content_type="text/markdown",
