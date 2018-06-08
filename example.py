d560cacc6275bc1c95ddec0c7e53fde283713ed3
hadrianl <137150224@qq.com>
2018-06-08 13:56:52 +0800

    例子一枚

---
+++ example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/8 0008 13:47
+# @Author  : Hadrianl 
+# @File    : example.py
+# @Contact   : 137150224@qq.com
+from huobitrade.service import HBWebsocket, HBRestAPI
+from huobitrade.handler import DBHandler
+from huobitrade import setKey
+import time
+setKey('access_key', 'secret_key')
+
+hb = HBWebsocket()
+hb.run()
+time.sleep(1)  # run之后连接需要一丢丢时间，sleep一下再订阅
+hb.sub_kline('ethbtc', '1min')
+handler = DBHandler()
+hb.register_handler(handler)
+
+@hb.register_handle_func('market.ethbtc.kline.1min')
+def handle(msg):
+    print('handle:', msg)
+
+api = HBRestAPI()
+print(api.get_timestamp())
