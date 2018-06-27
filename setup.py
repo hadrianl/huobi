2e0f637511d2a895b25bec019bd0e45d523d390e
hadrianl <137150224@qq.com>
2018-06-27 13:03:48 +0800

    0.3.1 --- DEUBG

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = ['websocket-client',
 hb_packages = ['huobitrade', 'huobitrade/extra']
 
 setup(name='huobitrade',
-      version='0.3.0',
+      version='0.3.1',
       description='huobi_api for python',
       long_description=long_description,
       long_description_content_type="text/markdown",
